6f307d4e5228737e5ed07cfaf41311b5fe3f1593
Silvana1409 <89832689+Silvana1409@users.noreply.github.com>
2022-12-02 11:40:23 +0100

    databindng

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -25,21 +25,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNickname(view:View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//       val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//
+//        nicknameTextView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
+//
+//        // Hide the keyboard.
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility =View.GONE
+            doneBu.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
-       // nicknameTextView.text = editText.text
-        binding.nicknameText.text = binding.nicknameEdit.text
-       // editText.visibility = View.GONE
-        binding.nicknameEdit.visibility =View.GONE
-       // view.visibility = View.GONE
-        binding.doneBu.visibility = View.GONE
-       // nicknameTextView.visibility = View.VISIBLE
-        binding.nicknameText.visibility = View.VISIBLE
-
-
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
